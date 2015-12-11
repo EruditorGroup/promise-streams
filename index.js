5d4db164cf58bf25ad3c244a05ca217c0818a4cd
Sokolov A <teopetuk@gmail.com>
2015-12-11 18:20:28 +0300

    PROFI-1863: do not exceed highwatermark

--- index.js
+++ index.js
@@ -51,9 +51,9 @@ PromiseStream.prototype._transform = incoming;
 function incoming(data, enc, done) {
     var queue = this._queue;
     var processed = Promise.resolve([data, enc])
-        .bind(this)
-        .spread(this._fn)
-        .then(nothing);
+      .bind(this)
+      .spread(this._fn)
+      .then(nothing);
     processed.catch(nothing);
     queue.push(processed);
     if (queue.length >= this._concurrent) {
@@ -80,11 +80,11 @@ function complete(done) {
     if (!this._end)
         this._end = nothing;
     Promise.all(this._queue)
-    .then(nothing)
-    .bind(this)
-    .then(this._end)
-    .then(this._finishUp)
-    .done(done, done);
+      .then(nothing)
+      .bind(this)
+      .then(this._end)
+      .then(this._finishUp)
+      .done(done, done);
 
 }
 
@@ -95,8 +95,8 @@ PromiseStream.prototype._finishUp = function() {
 PromiseStream.prototype.push = push;
 function push(data) {
     return Promise.cast(data)
-    .bind(this)
-    .then(Transform.prototype.push);
+      .bind(this)
+      .then(Transform.prototype.push);
 }
 
 PromiseStream.prototype.map = map;
@@ -114,7 +114,7 @@ function reduce(opts, fn, initial) {
 }
 
 PromiseStream.prototype.wait =
-PromiseStream.prototype.promise = promise;
+  PromiseStream.prototype.promise = promise;
 function promise() {
     return this._streamEnd.promise;
 }
@@ -130,6 +130,7 @@ function MapPromiseStream(opts, fn) {
     PromiseStream.call(this, opts, fn);
     this._mapfn = this._fn;
     this._fn = mapStreamFn;
+    wrapTransform(this);
 }
 
 function mapStreamFn(el) {
@@ -157,7 +158,7 @@ function ReducePromiseStream(opts, fn, initial) {
 }
 
 ReducePromiseStream.prototype.wait =
-ReducePromiseStream.prototype.promise = reduceStreamPromise;
+  ReducePromiseStream.prototype.promise = reduceStreamPromise;
 function reduceStreamPromise() {
     return this._reduceResult.promise;
 }
@@ -165,11 +166,11 @@ function reduceStreamPromise() {
 
 function reduceStreamFn(el, enc) {
     var initial = this._initial,
-        acc = this._acc;
+      acc = this._acc;
     if (acc === null)
         acc = typeof(initial) !== 'undefined'
-            ? Promise.cast(initial)
-            : Promise.cast(el);
+          ? Promise.cast(initial)
+          : Promise.cast(el);
     else
         acc = Promise.join(acc, el, enc).spread(this._reducefn);
     this._acc = acc;
@@ -179,7 +180,7 @@ function reduceStreamFn(el, enc) {
 
 function reduceStreamEnd() {
     return Promise.cast(this._acc)
-        .then(this._reduceResult.resolve);
+      .then(this._reduceResult.resolve);
 }
 
 //---------------------------
@@ -217,15 +218,15 @@ function pipe(source, sink) {
         resolve = resolve_;
         reject = reject_;
         source
-            .on("end", resolve)
-            .on("error", reject)
-            .pipe(sink)
-            .on("error", reject);
+          .on("end", resolve)
+          .on("error", reject)
+          .pipe(sink)
+          .on("error", reject);
     }).finally(function() {
-        source.removeListener("end", resolve);
-        //source.removeListener("error", reject);
-        //sink.removeListener("error", reject);
-    });
+          source.removeListener("end", resolve);
+          //source.removeListener("error", reject);
+          //sink.removeListener("error", reject);
+      });
 }
 
 //---------------------------
@@ -240,6 +241,147 @@ function pipeline() {
     return Promise.all(arr);
 }
 
+//---------------------------
+// wrap push, _transform
+//---------------------------
+
+/**
+ * Copy from
+ *  https://github.com/nodejs/node/blob/master/lib/_stream_transform.js
+ * @param state
+ * @returns {boolean}
+ */
+function needMoreData(state) {
+    return !state.ended &&
+      (state.needReadable ||
+      state.length < state.highWaterMark ||
+      state.length === 0);
+}
+
+/**
+ * Overrides Transform.push() to not exceed highWaterMark
+ * @param chunk
+ * @param encoding
+ * @param {booolean} flushOnly Ignore data -- just try to flush queue.
+ * @returns {boolean}
+ */
+function safePush(chunk, encoding, flushOnly) {
+    var state = this._readableState;
+    var queue = this.wrapTransformData.queue;
+    if(!flushOnly){ queue.push(chunk); }
+
+    var pushSt = needMoreData(state);
+    for(;pushSt && queue.length;) {
+        pushSt = this.wrapTransformData.push.apply(this, [
+            queue.shift(),
+            encoding
+        ]);
+    }
+    return pushSt;
+}
+
+/**
+ * Defer writeCallback until push queue is flushed and needMoreData() returns true
+ */
+function safeTransformCallback(self, err, data, cb) {
+    if(err) { return cb(err); }
+    if(data){ self.push(); }
+
+    var queue = self.wrapTransformData.queue;
+    var state = self._readableState;
+
+    if(needMoreData(state) && !queue.length){
+        return setImmediate(cb);
+    }
+
+    self.wrapTransformData.transformCallback = cb;
+    self.wrapTransformData.isDeferredCallback = true;
+}
+
+/**
+ * Overrides _transform to use safeTransformCallback() as callback
+ */
+function safeTransform(chunk, encoding, cb) {
+    //console.log("safeTransform", JSON.stringify(chunk));
+    var self = this;
+    this.wrapTransformData._transform.apply(this, [
+        chunk,
+        encoding,
+        function (error, data) {
+            return safeTransformCallback(self, error, data, cb);
+        }
+    ]);
+}
+
+/**
+ * When HighWaterMark is no more exceeded, we will try to flush push buffer
+ * and call writeCallback.
+ */
+function onLowWaterMark(self) {
+    if(!self.wrapTransformData.isDeferredCallback) { return; }
+
+    var pushSt = self.safePush(null, null, true);
+    if(pushSt) {
+        self.wrapTransformData.isDeferredCallback = false;
+        setImmediate(self.wrapTransformData.transformCallback);
+    }
+}
+
+/**
+ * Overrides _read() to emit lowWaterMark event.
+ */
+function readAndWaterMarkEvent(n) {
+    var readSt = this.wrapTransformData._read.apply(this, [n]);
+
+    var queue = this.wrapTransformData.queue;
+    var state = this._readableState;
+    //console.log("read", needMoreData(state));
+    var self = this;
+    return setImmediate(function () {
+        if(needMoreData(state)){
+            //console.log("read -> lowWaterMark");
+            self.emit('lowWaterMark');
+        }
+    });
+    return readSt;
+}
+
+/**
+ * Adjust Transform stream instance to not exceed highWaterMark.
+ * @param {Transform} self Transform stream instance
+ * @returns {*}
+ */
+function wrapTransform(self) {
+    self.wrapTransformData = {
+        isDeferredCallback: false,
+        queue: [],
+        push: self.push,
+        _read: self._read,
+        _transform: self._transform
+    };
+
+    self.push = self.safePush = safePush;
+    self._transform = safeTransform;
+
+    self._read = readAndWaterMarkEvent;
+
+    //var queue = self.wrapTransformData.queue;
+    //var state = self._readableState;
+    //setInterval(function () {
+    //  if(needMoreData(state)) {
+    //    return setImmediate(function () {
+    //      self.emit('lowWaterMark');
+    //    });
+    //  }
+    //}, 10);
+
+    self.on('lowWaterMark', function (data) {
+        return onLowWaterMark(self, data);
+    });
+
+    return self;
+}
+
 // API
 
 exports.through = PromiseStream;
@@ -249,3 +391,4 @@ exports.wait = waitStream;
 exports.pipe = pipe;
 exports.pipeline = pipeline;
 exports.collect = collect;
+exports.wrapTransform = wrapTransform;
